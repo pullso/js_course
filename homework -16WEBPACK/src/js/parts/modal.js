d387ee821847c357fee396a8a693b3e362bfaadc
Pavel Pullso <p.dubitskiy@ya.ru[remote origin]>
2020-02-01 10:10:02 +0500

    add webpack & react cases

---
+++ homework -16WEBPACK/src/js/parts/modal.js
@@ -0,0 +1,34 @@
+function modal() {
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close'),
+		decrBtn = document.querySelectorAll('.description-btn');
+
+	decrBtn.forEach(function(item) {
+		item.addEventListener('click', function() {
+			overlay.style.display = 'block';
+			this.classList.add('more-splash');
+			document.body.style.overflow = 'hidden';
+		});
+	});
+
+	more.addEventListener('click', function() {
+		overlay.style.display = 'block';
+		this.classList.add('more-splash');
+		document.body.style.overflow = 'hidden';
+	});
+
+	more.addEventListener('click', function() {
+		overlay.style.display = 'block';
+		this.classList.add('more-splash');
+		document.body.style.overflow = 'hidden';
+	});
+
+	close.addEventListener('click', function() {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+}
+
+module.exports = modal;
